2695f79692e1346c0f9d65c3044f15deb505113f
Dankó Dávid <info@dankodavid.hu>
2019-11-13 02:03:44 +0100

    build_tests.sh desn't need to include lazyctest.c

--- src/c/build_tests.sh
+++ src/c/build_tests.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#g++ -g -rdynamic -shared -fPIC utils.c lazyctest.c tests.cpp -o tests.so
-g++ -g -rdynamic -shared -fPIC lazyctest.c tests.cpp -o tests.so
+g++ -g -rdynamic -shared -fPIC tests.cpp -o tests.so
